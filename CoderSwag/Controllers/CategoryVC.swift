080f17ed2991065ef992e615baf37d55f0de3d02
James Volmert <james.volmert@gmail.com>
2018-09-23 17:45:02 -0600

    beginning ot wire up table view cells

---
+++ CoderSwag/Controllers/CategoryVC.swift
@@ -0,0 +1,16 @@
+
+
+import UIKit
+
+class CategoryVC: UIViewController {
+
+    @IBOutlet weak var CategoryTable: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
