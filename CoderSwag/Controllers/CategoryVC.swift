370eae3683705a3d4d8cc52527d01cd76766fd25
James Volmert <james.volmert@gmail.com>
2018-09-24 22:20:45 -0600

    modified table view cell

--- CoderSwag/Controllers/CategoryVC.swift
+++ CoderSwag/Controllers/CategoryVC.swift
@@ -2,13 +2,30 @@
 
 import UIKit
 
-class CategoryVC: UIViewController {
+class CategoryVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    
 
     @IBOutlet weak var CategoryTable: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        CategoryTable.dataSource = self
+        CategoryTable.delegate = self
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return DataService.instance.getCategories().count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell") as? CategoryCell {
+           let category = DataService.instance.getCategories()[indexPath.row]
+            cell.updateViews(category: category)
+            return cell
+        } else {
+            return CategoryCell()
+        }
     }
 
 
---
+++ CoderSwag/Models/Category.swift
@@ -0,0 +1,22 @@
+//
+//  Category.swift
+//  CoderSwag
+//
+//  Created by James Volmert on 9/23/18.
+//  Copyright © 2018 James Volmert. All rights reserved.
+//
+
+import Foundation
+
+struct Category {
+    private(set) public var title: String
+    private(set) public var imageName: String
+    
+    init(title: String, imageName: String) {
+        self.title = title
+        self.imageName = imageName
+    }
+    
+    
+    
+}
