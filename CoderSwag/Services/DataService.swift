9efb8d5f94db93bd2555fa7fa2562e632d54a2a3
James Volmert <james.volmert@gmail.com>
2018-10-23 19:36:08 -0600

    added category view

--- CoderSwag/Services/DataService.swift
+++ CoderSwag/Services/DataService.swift
@@ -10,6 +10,24 @@ class DataService {
         Category(title: "HATS", imageName: "hats.png"),
         Category(title: "DIGITAL", imageName: "digital.png")
     ]
+    
+    private let hats = [
+        Product(title: "Devslopes Logo Graphic Beanie", price: "25", imageName: "hat01.png"),
+        Product(title: "Devslopes Logo graphic hat", price: "33", imageName: "hat02.png"),
+        Product(title: "Devslopes Logo Hat White", price: "22.50", imageName: "hat03.png"),
+        Product(title: "Devslopes Logo snp back", price: "20", imageName: "hat04.png")
+    ]
+    
+    private let hoodies = [
+        Product(title: "Devslopes Logo Hoodie Gray", price: "34", imageName: "hoodie01.png"),
+        Product(title: "Devslopes Logo Hoodie black ", price: "88", imageName: "hoodie02.png"),
+        Product(title: "Devslopes Logo Gray", price: "27", imageName: "hoodie03.png"),
+        Product(title: "Devslopes Logo Black", price: "23", imageName: "hoodie04.png")
+    ]
+    
+    private let shirts = [
+        Product(title: <#T##String#>, price: <#T##String#>, imageName: <#T##String#>)
+    ]
     func getCategories() -> [Category] {
         return categories
         
